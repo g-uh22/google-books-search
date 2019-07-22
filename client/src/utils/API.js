68950e091b94941e47aaacd15bb7c029d0ec89eb
g-uh22 <gialeerobbins@gmail.com>
2019-07-21 21:11:04 -0700

    updated routes and comments

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export default {
-  // call to the Google Book API when searching for a book
+  // call to the Google Book API
   searchBooks: function(query) {
     return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
   },
 
-  // display the saved books from our database
+  // display the saved books from db
   getSavedBooks: function() {
     return axios.get("/api/books/saved" );
   },
-  // delete a saved book from our database
+  // delete a saved book from db
   deleteBook: function(id) {
     return axios.delete("/api/books/delete/" + id);
   },
 
-  // save the book in our database
+  // save the book in our db
   saveBook: function(bookKey, bookData) {
     return axios.post("/api/books/" + bookKey, bookData);
   }
